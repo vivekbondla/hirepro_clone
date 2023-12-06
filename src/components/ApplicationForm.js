91ed14201474b562e5aadbde03ddbd86a3f077ec
gollaparamesh <151898654+gollaparamesh@users.noreply.github.com>
2023-12-06 18:07:30 +0530

    modify a application form and resume upload

--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-
+import ResumeUpload from './ResumeUpload';
  
 const ApplicationForm = () => {
   const [formData, setFormData] = useState({
@@ -30,7 +30,7 @@ const ApplicationForm = () => {
  
   return (
     <div className="application-form-container">
-      <h2 className="application-form-title">Application Form</h2>
+      <h2 className="application-form-title">SUBMIT YOUR APPLICATION</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="firstName">First Name:</label>
@@ -76,16 +76,95 @@ const ApplicationForm = () => {
           />
         </div>
  
-        <div className="form-group">
-          <label htmlFor="address">Address:</label>
-          <textarea
-            id="address"
-            name="address"
-            value={formData.address}
+        
+        <div>
+          <lable htmlFor="Current Company">Current Company:</lable>
+          <textarea id='current Company'
+            name="Current Company"
+            value={formData.CurrentCompany}
             onChange={handleChange}
-          />
+            />
         </div>
+        <h2>LINKS</h2>
+        
+ <div className="form-group">
+   <label htmlFor="LinkedIn URL">LinkedIn URL:</label>
+   <textarea
+     id="LinkedIn URL"
+     name="LinkedIn URL"
+     value={formData.LinkedIn }
+     onChange={handleChange}
+   />
+ </div>
+ <h3>MANDATE QUESTIONNANRI</h3>
+ <div className="form-group">
+   <label htmlFor="Experince">Total Experince:</label>
+   <textarea
+     id="Experince"
+     name="Experince"
+     value={formData.Experince}
+     onChange={handleChange}
+   />
+ </div>
+
+ <div className="form-group">
+   <label htmlFor="Notice Period">Notice Period:</label>
+   <textarea
+     id="Notice Period"
+     name="Notice Period"
+     value={formData.NoticePeriod}
+     onChange={handleChange}
+   />
+ </div>
+
  
+ <div className="form-group">
+   <label htmlFor="Current CTC">Current CTC:</label>
+   <input
+    type='number'
+     id="Current CTC "
+     name="Current CTC"
+     value={formData.CurrentCTC}
+     onChange={handleChange}
+   />
+ </div>
+
+
+ <div className="form-group">
+   <label htmlFor="Expected CTC">Expected CTC:</label>
+   <input
+   type='number'
+     id="Expected CTC"
+     name="Expected CTC"
+     value={formData.ExpectedCTC}
+     onChange={handleChange}
+   />
+ </div>
+
+
+ <div className="form-group">
+   <label htmlFor="Current Location">Current Location:</label>
+   <textarea
+     id="Current Location"
+     name="Current Location"
+     value={formData.CurrentLocation}
+     onChange={handleChange}
+   />
+ </div>
+
+<h3>ADDITIONAL INFORMATION</h3>
+ <div className="form-group">
+   
+   <textarea
+     
+   />
+ </div>
+
+<div>
+  <ResumeUpload/>
+  
+</div>
+
         {/* ... existing form fields ... */}
  
         <button type="submit">Submit</button>
