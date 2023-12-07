db0c1631cb2ac5ed069526721ebd58eafdd4481e
SAI SPANDAN KURAPATI <104056615+spandan-kurapati@users.noreply.github.com>
2023-12-07 11:32:34 +0530

    Homepage-Logo Feature has been added with some extra info in Appplication form

--- src/UI/NavBar.js
+++ src/UI/NavBar.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
-
+ 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="name">
-        <img src="https://static.wixstatic.com/media/c5d8c7_64210ef5598948089634336b2dc3923c~mv2.png/v1/fill/w_348,h_60,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1.png" alt="Innogen Logo" className="logo" />
+       
+        <Link to="/">
+          <img
+            src="https://static.wixstatic.com/media/c5d8c7_64210ef5598948089634336b2dc3923c~mv2.png/v1/fill/w_348,h_60,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1.png"
+            alt="Innogen Logo"
+            className="logo"
+          />
+        </Link>
       </div>
       <div className="button_div">
         <button className="button">Find Jobs</button>
@@ -13,5 +21,5 @@ const NavBar = () => {
     </nav>
   );
 };
-
+ 
 export default NavBar;
--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -12,8 +12,16 @@ const ApplicationForm = () => {
     dateOfBirth: '',
     programmingLanguages: '',
     skills: '',
+    currentCompany: '',
+    linkedInURL: '',
+    totalExperience: '',
+    noticePeriod: '',
+    currentCTC: '',
+    expectedCTC: '',
+    currentLocation: '',
+    additionalInformation: '',
   });
- 
+  
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -86,7 +94,91 @@ const ApplicationForm = () => {
           />
         </div>
  
-        {/* ... existing form fields ... */}
+        <div className="form-group">
+          <label htmlFor="currentCompany">Current Company:</label>
+          <input
+            type="text"
+            id="currentCompany"
+            name="currentCompany"
+            value={formData.currentCompany}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="linkedInURL">LinkedIn URL:</label>
+          <input
+            type="text"
+            id="linkedInURL"
+            name="linkedInURL"
+            value={formData.linkedInURL}
+            onChange={handleChange}
+          />
+        </div>
+        <h3>MANDATE QUESTIONNANRI</h3>
+ <div className="form-group">
+   <label htmlFor="Experince">Total Experince:</label>
+   <textarea
+     id="Experince"
+     name="Experince"
+     value={formData.Experince}
+     onChange={handleChange}
+   />
+ </div>
+ 
+ <div className="form-group">
+   <label htmlFor="Notice Period">Notice Period:</label>
+   <textarea
+     id="Notice Period"
+     name="Notice Period"
+     value={formData.NoticePeriod}
+     onChange={handleChange}
+   />
+ </div>
+ 
+ 
+ <div className="form-group">
+   <label htmlFor="Current CTC">Current CTC:</label>
+   <input
+    type='number'
+     id="Current CTC "
+     name="Current CTC"
+     value={formData.CurrentCTC}
+     onChange={handleChange}
+   />
+ </div>
+ 
+ 
+ <div className="form-group">
+   <label htmlFor="Expected CTC">Expected CTC:</label>
+   <input
+   type='number'
+     id="Expected CTC"
+     name="Expected CTC"
+     value={formData.ExpectedCTC}
+     onChange={handleChange}
+   />
+ </div>
+ 
+ 
+ <div className="form-group">
+   <label htmlFor="Current Location">Current Location:</label>
+   <textarea
+     id="Current Location"
+     name="Current Location"
+     value={formData.CurrentLocation}
+     onChange={handleChange}
+   />
+ </div>
+ 
+<h3>ADDITIONAL INFORMATION</h3>
+ <div className="form-group">
+   
+   <textarea
+     
+   />
+ </div>
+
  
         <button type="submit">Submit</button>
       </form>
